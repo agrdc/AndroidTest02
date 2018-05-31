34da6e8fa63b7e377aa508576055c78dbc00ce98
Agnaldo Rodrigues <agnaldordcn@gmail.com>
2018-05-31 17:10:55 -0300

    finishing contact fragment layout structure, starting message sent fragment layout structure

--- app/src/main/java/bilulo/androidtest02/ui/MessageSentFragment.java
+++ app/src/main/java/bilulo/androidtest02/ui/MessageSentFragment.java
@@ -6,18 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import bilulo.androidtest02.R;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ContactsFragment extends Fragment {
+public class MessageSentFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_contact, container,false);
+        View rootView = inflater.inflate(R.layout.fragment_message_sent, container,false);
         ButterKnife.bind(this,rootView);
         return rootView;
     }
